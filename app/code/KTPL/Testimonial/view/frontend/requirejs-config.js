99de9bc612193a618afc54179416d72a782ad3ec
bichukaleh <heena.bichukale@krishtechnolabs.com>
2019-09-27 18:56:41 +0530

    add slider in home page

---
+++ app/code/KTPL/Testimonial/view/frontend/requirejs-config.js
@@ -0,0 +1,10 @@
+var config = {
+    "map": {
+        "*": {
+            "OwlCarousel": "KTPL_Testimonial/js/owl-carousel"
+        }
+    },
+    "shim":{
+        "KTPL_Testimonial/js/owl.carousel.min": ["jquery"]
+    }
+};
